e784105b34dcea06781504bdc0d2c336225cf2ff
Corey Theiss <corey.theiss@maclaren.com>
2013-10-21 16:33:43 -0400

    Forgot to update version number.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ from setuptools import setup
 
 setup(
     name='simple-requests',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/ctheiss/simple-requests',
     license='MIT',
     author='Corey Theiss',
